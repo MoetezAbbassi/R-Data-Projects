e6346a7834735994a77e13fff64839ddfc19eb15
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-07 18:59:55 +0100

    Network Analysis update

--- Social Network Analysis/Social Network Analysis/Network Analysis.R
+++ Social Network Analysis/Social Network Analysis/Network Analysis.R
@@ -42,3 +42,32 @@ closeness(g1, mode='all', weights=NA)
 
 betweenness(g1, directed=T, weights=NA)
 
+#Reading datset:
+install.packages("RCurl")
+library(RCurl)
+
+data <- read.csv(text=getURL("https://raw.githubusercontent.com/MoetezAbbassi/R-Data-Projects/refs/heads/main/Social%20Network%20Analysis/Social%20Network%20Analysis/networkdata.csv"), header=TRUE)
+y<-data.frame(data$first, data$second)
+
+#Creating a network:
+net<-graph.data.frame(y, directed=T)
+V(net)
+E(net)
+V(net)$label <- V(net)$name
+V(net)$degree <- degree(net)
+
+#Histogram of node tree:
+
+hist(V(net)$degree,
+     col='green',
+     main= 'Histogram of Node Degree',
+     ylab= 'Frequency',
+     xlab= 'Degree of Vertices')
+#Network diagram:
+set.seed(222)
+plot(net,
+     vertex.color='green',
+     vertext.size=2,
+     vertex.label.dist=1.5,
+     edge.arrow.size=0.1,
+     vertex.label.cex=0.8)
