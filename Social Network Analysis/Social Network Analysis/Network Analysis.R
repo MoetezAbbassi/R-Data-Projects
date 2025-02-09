41c39e95688c3b4842852480d29d1c88d176f02f
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-09 10:15:01 +0100

    Added Hub&Autorities measures and Community Detection Feature

--- Social Network Analysis/Social Network Analysis/Network Analysis.R
+++ Social Network Analysis/Social Network Analysis/Network Analysis.R
@@ -71,3 +71,57 @@ plot(net,
      vertex.label.dist=1.5,
      edge.arrow.size=0.1,
      vertex.label.cex=0.8)
+
+
+#Highlighting degrees & layouts:
+plot(net,
+     vertex.color= rainbow(52),
+     vertex.size=V(net)$degree*0.4,
+     edge.arrow.size=0.1,
+     layout=layout.fruchterman.reingold)
+
+
+plot(net,
+     vertex.color= rainbow(52),
+     vertex.size=V(net)$degree*0.4,
+     edge.arrow.size=0.1,
+     layout=layout.graphopt)
+
+
+plot(net,
+     vertex.color= rainbow(52),
+     vertex.size=V(net)$degree*0.4,
+     edge.arrow.size=0.1,
+     layout=layout.kamada.kawai)
+
+
+#Hub and Autorities:
+hs <-hub_score(net)$vector
+as <-authority.score(net)$vector
+par(mfrow=c(1,2))
+
+set.seed(123)
+plot(net,
+     vertex.size=hs*30,
+     main ='Hubs',
+     vertex.color=rainbow(52),
+     edge.arrow.size=0.1,
+     layout= layout.kamada.kawai)
+
+plot(net,
+     vertex.size=as*30,
+     main ='Authorities',
+     vertex.color=rainbow(52),
+     edge.arrow.size=0.1,
+     layout= layout.kamada.kawai)
+
+par(mfrow=c(1,1))
+
+#Community detection:
+net<- graph.data.frame(y, directed=F)
+
+cnet <- cluster_edge_betweenness(net)
+plot(cnet,
+     net,
+     vertex.size=10,
+     vertex.label.cex=0.8)
