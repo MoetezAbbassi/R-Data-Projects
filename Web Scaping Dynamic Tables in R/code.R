23adef383ea3dacfc9af822cf98b9c0fca7ec886
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-12 11:02:30 +0100

    importing web scraping libraries

---
+++ Web Scaping Dynamic Tables in R/code.R
@@ -0,0 +1,12 @@
+install.packages("tidyverse")
+install.packages("RSelenium")
+install.packages("rvest")
+install.packages("netstat")
+install.packages("data.table")
+#loading packages:
+
+library(tidyverse) #data wrangling
+library(RSelenium) #activate Selenimu server
+library(rvest) # web scrape tables
+library(netstat) # find unused port
+library(data.table) #rbindlist function
