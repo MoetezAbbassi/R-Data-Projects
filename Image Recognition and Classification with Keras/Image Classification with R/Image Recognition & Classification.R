1bd69141278d1824ccf3825246a6b95d19c643a9
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-14 22:36:44 +0100

    One Hot Encoding

--- Image Recognition and Classification with Keras/Image Classification with R/Image Recognition & Classification.R
+++ Image Recognition and Classification with Keras/Image Classification with R/Image Recognition & Classification.R
@@ -15,4 +15,35 @@ mypic <- list()
 
 for(i in 1:12) { mypic[[i]] <- readImage(pics[i])}
 
+#Exploring
 print(mypic[[1]])
+display(mypic[[8]])
+summary(mypic[[1]])
+hist(mypic[[8]])
+str(mypic)
+
+#Resizing images(to 28x28x3):
+
+for(i in 1:12) {mypic[[i]] <- resize(mypic[[i]], 28, 28)}
+
+
+#Reshaping images:
+for (i in 1:12) { mypic[[i]] <- array_reshape(mypic[[i]], c(28,28,3))}
+
+str(mypic)
+
+
+#row bind:
+
+trainx <- NULL
+for (i in 1:5) {trainx <- rbind(trainx, mypic[[i]])}
+str(trainx)
+
+testx <- rbind(mypic[[6]], mypic[[12]])
+trainy <- c(0,0,0,0,0,1,1,1,1,1)
+testy <- c(0,1)
+
+#one hot encoding:
+
+trainLabels <- to_categorical(trainy)
+testLabels <- to_categorical(testy)
