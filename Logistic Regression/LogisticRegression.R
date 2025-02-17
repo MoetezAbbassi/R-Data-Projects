496f8a951b5633ea9a8fdac4834d542c7f176260
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-17 22:35:16 +0100

    Reading data and transformation into factors

--- Logistic Regression/LogisticRegression.R
+++ Logistic Regression/LogisticRegression.R
@@ -1,3 +1,12 @@
 #Goal is to classify the admission of student applications:
 
 #Loading data:
+
+mydata <- read.csv(file.choose(), header = T)
+str(mydata)
+
+#transforming our variables to factor/categorical variables
+mydata$admit<- as.factor(mydata$admit)
+mydata$rank <- as.factor(mydata$rank)
+
+xtabs(~admit + rank, data=mydata)
