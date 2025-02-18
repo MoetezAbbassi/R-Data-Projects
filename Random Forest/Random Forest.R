c84fed59f2b1d685b84765fc085ac9755eb8aa64
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-18 19:26:34 +0100

    Splitting Data

--- Random Forest/Random Forest.R
+++ Random Forest/Random Forest.R
@@ -3,3 +3,9 @@ str(data)
 
 data$NSP <- as.factor(data$NSP)
 table(data$NSP)
+
+#Splitting the data:
+set.seed(123)
+ind <- sample(2, nrow(data), replace=TRUE, prob= c(0.7, 0.3))
+train<- data[ind==1,]
+test<- data[ind==2,]
