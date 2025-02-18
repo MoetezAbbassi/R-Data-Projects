c31759d3a6fed97842dc558a79fa12615e6ca0d0
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-18 09:25:07 +0100

    Model Development & Evaluation

--- Logistic Regression/LogisticRegression.R
+++ Logistic Regression/LogisticRegression.R
@@ -9,4 +9,26 @@ str(mydata)
 mydata$admit<- as.factor(mydata$admit)
 mydata$rank <- as.factor(mydata$rank)
 
+#creating a contingency table:
 xtabs(~admit + rank, data=mydata)
+
+#partitioning data: 80% training - 20% testing:
+
+set.seed(1234)
+ind <- sample(2, nrow(mydata), replace = T, prob= c(0.8, 0.2))
+train <- mydata[ind==1,]
+test <- mydata[ind==2,]
+
+#Making Logistic regression model:
+mymodel <- glm(admit ~ gre + gpa + rank, data= train, family= 'binomial')
+summary(mymodel)
+
+#dropping gre variable because it has high p-value
+mymodel <- glm(admit ~ gpa + rank, data= train, family= 'binomial')
+summary(mymodel)
+
+#Prediction:
+p1 <- predict(mymodel, train, type="response")
+head(p1)
+head(train)
+
