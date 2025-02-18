8563361622a0c77ba357751625cdc5cae4f18c32
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-18 19:31:00 +0100

    Random Forest

--- Random Forest/Random Forest.R
+++ Random Forest/Random Forest.R
@@ -9,3 +9,14 @@ set.seed(123)
 ind <- sample(2, nrow(data), replace=TRUE, prob= c(0.7, 0.3))
 train<- data[ind==1,]
 test<- data[ind==2,]
+
+#Random Forest:
+
+library(randomForest)
+set.seed(222)
+rf <- randomForest(NSP~., data=train)
+print(rf)
+
+
+attributes(rf)
+rf$confusion
