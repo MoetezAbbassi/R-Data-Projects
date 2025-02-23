da9f7397cac6b061b1ea901f072c3ec36fed0bdc
Moatez Abbassi <abbassimoetez@gmail.com>
2025-02-23 20:59:37 +0100

    Data Visualization

--- Naive Bayes in R/Naive Bayes in R/Naive Bayes.R
+++ Naive Bayes in R/Naive Bayes in R/Naive Bayes.R
@@ -12,3 +12,14 @@ library(psych)
 
 #reading data
 df= read.csv(file.choose(), header=T)
+str(df)
+df$rank <- as.factor(df$rank)
+df$admit <- as.factor(df$admit)
+
+#visualization:
+pairs.panels(df[-1])
+
+df %>%
+  ggplot(aes(x=admit, y=gre, fill=admit))+
+  geom_boxplot()+
+  ggtitle("Box Plot")
